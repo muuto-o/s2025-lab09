43da14e58e3579801b8fc893d2ff6bbc28eec1ed
lucicul <unmnkh@gmail.com>
2025-04-24 20:21:41 +0800

    done

--- src/index.ts
+++ src/index.ts
@@ -54,14 +54,41 @@ function main (fileNames: string[]): void {
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+    console.log();
+    console.log("ASYNC/AWAIT IMPLEMENTATION");
+    for (const fileName of fileNames) {
+        console.log(`Running logo detection on ${fileName}`);
+        try {
+            const [result] = await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description)
+                    console.log(`"${logo.description}" found in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            if (scores.length > 0) {
+                const avg = scores.reduce((a, b) => a + b) / scores.length;
+                console.log(`Average score for ${fileName}: ${avg}`);
+            } else {
+                console.log(`No logos found in ${fileName}`);
+            }
+        } catch (err: any) {
+            if (err.code === 'ENOENT') {
+                console.log(`File ${fileName} not found`);
+            } else {
+                console.error(`Error processing ${fileName}:`, err);
+            }
+        }
+    }
 }
+
 
 main([
     './images/cmu.jpg', 
     './images/logo-types-collection.jpg', 
-    './images/not-a-file.jpg'
-]);
+]);  
 
 mainAsync([
     './images/cmu.jpg', 
